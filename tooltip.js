2f5f2d8038c271666006994da70c0ee0130ed8fb
William <stewart-w24@hotmail.co.uk>
2021-07-11 10:34:42 +0100

    fix to avoid tooltip styling clash with any global styles

--- tooltip.js
+++ tooltip.js
@@ -1,34 +1,38 @@
 class Tooltip extends HTMLElement {
-    constructor(){
+    constructor() {
         super(); //calls the constructor of the base class.
         this._tooltipContainer;
         this._tooltipText = 'Some dummy default tootip text if text attribute not set.';
+        this.attachShadow({ mode: 'open' }); //this adds shadow dom tree
+        this.shadowRoot.innerHTML = `
+            <slot>Some default</slot>
+            <span> (?)</span>
+        `;
     }
 
-    connectedCallback(){
-        if(this.hasAttribute('text')){
+    connectedCallback() {
+        if (this.hasAttribute('text')) {
             this._tooltipText = this.getAttribute('text');
         }
-        const tooltipIcon = document.createElement('span');
-        tooltipIcon.textContent = ' (?)';
+        const tooltipIcon = this.shadowRoot.querySelector('span');
         tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
         tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
-        this.appendChild(tooltipIcon);
+        this.shadowRoot.appendChild(tooltipIcon);
         this.style.position = 'relative';
     }
 
-    _showTooltip(){
+    _showTooltip() {
         this._tooltipContainer = document.createElement('div');
         this._tooltipContainer.textContent = this._tooltipText;
         this._tooltipContainer.style.backgroundColor = 'black';
         this._tooltipContainer.style.color = 'white';
         this._tooltipContainer.style.position = 'absolute';
         this._tooltipContainer.style.zIndex = '10';
-        this.appendChild(this._tooltipContainer);
+        this.shadowRoot.appendChild(this._tooltipContainer);
     }
 
-    _hideTooltip(){
-        this.removeChild(this._tooltipContainer);
+    _hideTooltip() {
+        this.shadowRoot.removeChild(this._tooltipContainer);
     }
 }
 
