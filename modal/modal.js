606259ab8f027af6896b19285edcdf66e28728e6
William <stewart-w24@hotmail.co.uk>
2021-07-17 14:36:11 +0100

    added opening logic and checks

--- modal/modal.js
+++ modal/modal.js
@@ -1,7 +1,8 @@
 class Modal extends HTMLElement {
-    constructor(){
+    constructor() {
         super();
         this.attachShadow({ mode: 'open' });
+        this.isOpen = false;
         this.shadowRoot.innerHTML = `
             <style>
                 #backdrop {
@@ -77,22 +78,26 @@ class Modal extends HTMLElement {
         `;
     }
 
-    // attributeChangedCallback(name, oldValue, newValue) {
-    //     // if(name === 'opened'){
-    //     //     if(this.hasAttribute('opened')){
-    //     //         this.shadowRoot.querySelector('#backdrop').style.opacity = 1;
-    //     //         this.shadowRoot.querySelector('#backdrop').style.pointerEvents = 'all';
-    //     //         this.shadowRoot.querySelector('#modal').style.opacity = 1;
-    //     //         this.shadowRoot.querySelector('#modal').style.pointerEvents = 'all';
-    //     //     }
-    //     // }
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (this.hasAttribute('opened')) {
+            this.isOpen = true;
+            //         this.shadowRoot.querySelector('#backdrop').style.opacity = 1;
+            //         this.shadowRoot.querySelector('#backdrop').style.pointerEvents = 'all';
+            //         this.shadowRoot.querySelector('#modal').style.opacity = 1;
+            //         this.shadowRoot.querySelector('#modal').style.pointerEvents = 'all';
+        } else {
+            this.isOpen = false;
+        }
+    }
 
-    // }
+    static get observedAttributes() {
+        return ['opened'];
+    }
 
-    // static get observedAttributes() {
-    //     return ['opened'];
-    // }
-
+    open() {
+        this.setAttribute('opened', '');
+        this.isOpen = true;
+    }
 }
 
 customElements.define('ws-modal', Modal);
