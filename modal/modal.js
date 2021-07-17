cc4315ab2c22a9f05789faa4de38c503f94ca2a6
William <stewart-w24@hotmail.co.uk>
2021-07-17 16:04:54 +0100

    added functionality to listen to custom events ('cancel' and 'confirm') using two different ways of registering the customer events

--- modal/modal.js
+++ modal/modal.js
@@ -114,12 +114,16 @@ class Modal extends HTMLElement {
         this.isOpen = false;
     }
 
-    _cancel() {
+    _cancel(event) {
         this.hide();
+        const cancelEvent = new Event('cancel', { bubbles: true, composed: true });
+        event.target.dispatchEvent(cancelEvent);
     }
 
     _confirm() {
         this.hide();
+        const confirmEvent = new Event('confirm');
+        this.dispatchEvent(confirmEvent);
     }
 }
 
