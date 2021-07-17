be1ae9ff2b49f7eda2069024f12b866a4b6e8638
William <stewart-w24@hotmail.co.uk>
2021-07-17 14:44:37 +0100

    added named slot to header and a default for when no slot is passed to the component

--- modal/modal.js
+++ modal/modal.js
@@ -43,7 +43,7 @@ class Modal extends HTMLElement {
                     padding: 1rem;
                 }
 
-                header h1 {
+                ::slotted(h1) {
                     font-size: 1.25rem;
                 }
 
@@ -65,7 +65,7 @@ class Modal extends HTMLElement {
             <div id="backdrop"></div>
             <div id="modal">
                 <header>
-                    <h1>Please confirm</h1>
+                    <slot name="title">Please confirm payment</slot>
                 </header>
                 <section id="main">
                     <slot></slot>
